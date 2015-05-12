c701f5448144a41fe6e2ed87cb522f69b1fca34c
Daryl Fritz <darylfritz@gmail.com>
2015-05-11 20:38:19 -0400

    Fix series migration

--- db/migrate/20150508005131_create_series.rb
+++ db/migrate/20150508005131_create_series.rb
@@ -2,7 +2,7 @@ class CreateSeries < ActiveRecord::Migration
   def change
     create_table :series do |t|
       t.string :name, null: false
-      t.references :series, index: true
+      t.references :publishers, index: true
       t.timestamps null: false
     end
 
