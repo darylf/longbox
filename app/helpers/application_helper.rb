a58dcd7578b7b0411f720fc7002a3241ef35340d
Daryl Fritz <darylfritz@gmail.com>
2018-07-06 16:18:18 -0400

    Fix screens and add title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Application helper
 module ApplicationHelper
+  def page_title
+    if content_for?(:page_title)
+      subtitle = " | #{content_for(:page_title)}"
+    end
+
+    "Longbox#{subtitle}"
+  end
 end
