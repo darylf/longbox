535f5215b71b827c4f4dc54ed8727b3b7ab72872
Daryl Fritz <darylfritz@gmail.com>
2020-09-29 00:11:22 -0400

    Update shoulda_matchers

--- api/spec/models/publisher_spec.rb
+++ api/spec/models/publisher_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:publisher) { build(:publisher) }
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(publisher).to be_valid
+    end
+
+    it { should validate_presence_of(:name) }
+
+    it { should validate_length_of(:name).is_at_most(120) }
+  end
 end
--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -30,6 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -61,4 +64,15 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Configure RSpec to include factory_bot methods
+  config.include FactoryBot::Syntax::Methods
+end
+
+# Configure shoulda-matchers to work with rspec and all of rails.
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
