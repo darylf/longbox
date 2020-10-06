278e4536bb7e9f3769e9c851cbead12d4e02495d
Daryl Fritz <darylfritz@gmail.com>
2020-10-05 23:41:38 -0400

    Eject create-react-app; Configure webpack

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
-import React from "react";
+import * as React from "react";
 
-function App() {
-  return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
-  );
-}
-
-export default App;
+export const App  = (
+  <h1>
+    Hi World
+  </h1>
+);
---
+++ client/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.tsx"),
+  devtool: 'inline-source-map',
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
+  },
+};
