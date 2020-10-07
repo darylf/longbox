71b0be51a4785b3b2b981f382a7efbf618e9bdd6
Daryl Fritz <darylfritz@gmail.com>
2020-10-07 15:51:17 -0400

    Refactor webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,40 +1,39 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.tsx"),
-  devtool: "inline-source-map",
-  mode: "development",
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js']
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: 'src/index.html'
     }),
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin()
   ],
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
   },
   devServer: {
     port: 3000,
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true,
-  },
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
+  }
 };
