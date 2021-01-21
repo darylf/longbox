9e57e29f6a1ea0c6ccf7a21a4e4547fd10d13679
Daryl Fritz <darylfritz@gmail.com>
2021-01-20 22:26:24 -0500

    Fix rubocop

--- api/app/graphql/types/book_attributes_type.rb
+++ api/app/graphql/types/book_attributes_type.rb
@@ -1,8 +1,10 @@
 module Mutations
-  class Types::BookAttributesType < Types::BaseInputObject
-    argument :alternate_title, String, required: false
-    argument :credit_ids, [ID], required: false
-    argument :issue, String, required: false
-    argument :series_id, ID, required: false
+  module Types
+    class BookAttributesType < Types::BaseInputObject
+      argument :alternate_title, String, required: false
+      argument :credit_ids, [ID], required: false
+      argument :issue, String, required: false
+      argument :series_id, ID, required: false
+    end
   end
 end
