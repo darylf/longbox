dcfc3931fae6476dea5157539596c89d3383c3ef
Daryl Fritz <darylfritz@gmail.com>
2021-06-09 17:30:04 -0400

    Restructure UI config

---
+++ web/src/__tests__/setupTests.ts
@@ -0,0 +1,6 @@
+// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+// import enzyme from 'enzyme';
+
+// enzyme.configure({ adapter: new Adapter() });
+
+export const TODO = "write tests";
