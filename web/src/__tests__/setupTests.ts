7afa2039ceb29098c5cc158fab50f060372897f4
Daryl Fritz <darylfritz@gmail.com>
2021-06-10 16:47:41 -0400

    Cleanup

--- web/src/__tests__/setupTests.ts
+++
@@ -1,6 +0,0 @@
-// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-// import enzyme from 'enzyme';
-
-// enzyme.configure({ adapter: new Adapter() });
-
-export const TODO = "write tests";
