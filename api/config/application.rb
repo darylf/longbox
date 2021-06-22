80f71963c3516a3c13e7a500784ff4e6d1868b43
Daryl Fritz <darylfritz@gmail.com>
2021-06-22 16:24:06 -0400

    Add graphiql for development

--- api/config/application.rb
+++ api/config/application.rb
@@ -11,7 +11,7 @@ require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 # require "action_view/railtie"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
